0df8ebc6eb28efe3615f7fa36b008967ae1cedf6
saikumarmarripally <saikumarmarripally38@gmail.com>
2023-10-10 02:53:41 +0000

    commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const element = (
+  <div>
+    <h1 className="greeting"> Hi sitamahalaxmi </h1>
+    <p>Good to see you </p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
+
+{
+  /* 
+
+    USE ONLY ONE METHOD
+
+           <script type="text/babel" src="index.js"></script>
+
+           indexjs file contain.....
+           const element = (
+               <div>
+                   <h1 className="greeting"> Hi sitamahalaxmi </h1>
+                    <p>Good to see you </p>
+               </div>
+            );
+            ReactDOM.render(element, document.getElementById("root"));
+
+            {
+
+           (or)
+
+           <script type = "text/babel">
+
+                    const element = <h1 className = "greeting"> SitaMahalaxmi</h1>
+                     ReactDOM.render(element, document.getElementById("root"));
+           </script>
+*/
+}
